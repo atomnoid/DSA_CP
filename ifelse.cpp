c3842217ff70492b9a89b56b28a20101c67bef17
atomnoid <aayushs2580@gmail.com>
2026-04-21 13:19:40 +0530

    positive and negative number checking using c

---
+++ ifelse.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 45;
+    
+    if (n>=0) {
+        cout << "Positive number" << endl;
+    }
+    else {
+        cout << "Negative number" << endl;
+    }
+}
