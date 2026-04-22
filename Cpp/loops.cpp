128300fe367eb119be7b1797c06f2983f7db1b4c
atomnoid <aayushs2580@gmail.com>
2026-04-22 23:39:12 +0530

    made a loop program

---
+++ Cpp/loops.cpp
@@ -0,0 +1,12 @@
+// a basic loop program in C++
+
+#include <iostream>
+using namespace std;        
+int main(){
+    int n;
+    cout<<"enter a number: ";
+    cin>>n;
+    for (int i=1; i<=10; i++){
+        cout<<n<<" x "<<i<<" = "<<n*i<<endl;
+    }
+}
