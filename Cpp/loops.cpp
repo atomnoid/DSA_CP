f67c6b17092cc2cb4573024d04b9eea4a64520b5
atomnoid <aayushs2580@gmail.com>
2026-04-23 23:54:11 +0530

    code

--- Cpp/loops.cpp
+++ Cpp/loops.cpp
@@ -10,3 +10,16 @@ int main(){
         cout<<n<<" x "<<i<<" = "<<n*i<<endl;
     }
 }
+
+#include <iostream>
+using namespace std;    
+int main(){
+    int n;
+    cout<<"enter a number: ";
+    cin>>n;
+    int i=1;
+    while (i<=10){
+        cout<<n<<" x "<<i<<" = "<<n*i<<endl;
+        i++;
+    }
+}
